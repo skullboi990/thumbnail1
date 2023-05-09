f8ba46cdb136db41c459a561b9c4564c9538e322
skullboi990 <132878139+skullboi990@users.noreply.github.com>
2023-05-09 02:07:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
-  }
-};
+let direction = 0.5;
+let angle = 0;
 
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+function rotate() {
+  const modelViewer = document.querySelector('model-viewer');
+  angle += direction;
+  if (angle > 50) {
+    direction = -0.5;
+  } else if (angle < -50) {
+    direction = 0.5;
+  }
+  modelViewer.setAttribute('camera-orbit', `${angle}deg 75deg auto`);
+  requestAnimationFrame(rotate);
+}
 
 window.addEventListener('load', (event) => {
   const modelViewer = document.querySelector('model-viewer');
@@ -22,20 +22,7 @@ window.addEventListener('load', (event) => {
     cancelable: true
   });
   modelViewer.dispatchEvent(click);
+
+  // Start the rotation
+  rotate();
 });
-let direction = 1;
-let angle = 0;
-
-function rotate() {
-  const modelViewer = document.querySelector('model-viewer');
-  angle += direction;
-  if (angle > 50) {
-    direction = -1;
-  } else if (angle < -50) {
-    direction = 1;
-  }
-  modelViewer.cameraOrbit = `${angle}deg 75deg auto`;
-  requestAnimationFrame(rotate);
-}
-
-requestAnimationFrame(rotate);
