b8f110b11965c3f60564e57a9c32fb4f4e2d7228
skullboi990 <132878139+skullboi990@users.noreply.github.com>
2023-05-09 02:18:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-let direction = 0.5;
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+
+const modelViewer = document.querySelector('model-viewer');
+modelViewer.addEventListener('progress', onProgress);
+
+// Start the rotation
+let direction = 0.1; // change this value to adjust rotation speed
 let angle = 0;
 
 function rotate() {
   const modelViewer = document.querySelector('model-viewer');
   angle += direction;
   if (angle > 50) {
-    direction = -0.5;
+    direction = -0.1;
   } else if (angle < -50) {
-    direction = 0.5;
+    direction = 0.1;
   }
   modelViewer.setAttribute('camera-orbit', `${angle}deg 75deg auto`);
   requestAnimationFrame(rotate);
@@ -15,6 +32,7 @@ function rotate() {
 
 window.addEventListener('load', (event) => {
   const modelViewer = document.querySelector('model-viewer');
+
   // Dispatch synthetic click event
   const click = new MouseEvent('click', {
     view: window,
@@ -26,3 +44,10 @@ window.addEventListener('load', (event) => {
   // Start the rotation
   rotate();
 });
+You can adjust the value of direction to fine-tune the rotation speed to your liking. Remember, the smaller the value, the slower the rotation.
+
+
+
+
+
+
